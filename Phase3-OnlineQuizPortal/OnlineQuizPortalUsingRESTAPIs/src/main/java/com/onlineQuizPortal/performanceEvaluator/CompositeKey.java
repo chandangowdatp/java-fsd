a126b6fc13f5e80038fca397cdf39f6e837d61aa
Chandangowda T P <102135018+chandangowdatp@users.noreply.github.com>
2022-09-13 14:04:18 +0530

    Delete Phase3-OnlineQuizPortal/OnlineQuizPortalUsingRESTAPIs directory

--- Phase3-OnlineQuizPortal/OnlineQuizPortalUsingRESTAPIs/src/main/java/com/onlineQuizPortal/performanceEvaluator/CompositeKey.java
+++
@@ -1,9 +0,0 @@
-package com.onlineQuizPortal.performanceEvaluator;
-
-import java.io.Serializable;
-
-
-public class CompositeKey implements Serializable {     // to make a composite primary key
-    private int userId;
-    private int quizId;
-}
--- Phase3-OnlineQuizPortal/OnlineQuizPortalUsingRESTAPIs/src/main/java/com/onlineQuizPortal/question/QuestionJPARepo.java
+++
@@ -1,9 +0,0 @@
-package com.onlineQuizPortal.question;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-@Repository
-public interface QuestionJPARepo extends JpaRepository<QuestionObject, Integer> {
-}
